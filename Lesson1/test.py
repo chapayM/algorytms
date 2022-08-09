cde800a36197517205d340441bcf014389491827
chapayM <106312222+chapayM@users.noreply.github.com>
2022-08-09 10:57:34 +0300

    Information
    Скрины из лекционных материалов

--- Lesson1/test.py
+++ Lesson1/test.py
@@ -1,5 +1,33 @@
-year = int(input())
-if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
-    print('YES')
-else:
-    print('NO')
+# year = int(input())
+# if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
+#     print('YES')
+# else:
+#     print('NO')
+import random
+
+my_typle = [[random.randint(1, 10) for _ in range(5)] for _ in range(7)]
+print((my_typle))
+for column in my_typle:
+    for item in column:
+        print(f'{item:>4}', end='')
+    print()
+
+# def gcb(a, b):
+#     if b == 0:
+#         return a
+#     return gcb(b, a%b)
+#
+# print(gcb(12, 6))
+# a=b=0
+# n = 1
+# while n>0:
+#     if n%2==0:
+#         a+=1
+#     else:
+#         b+=1
+#     n = n//10
+# print(a, b)
+
+
+
+
--- Lesson9(hash)/Task4.py
+++ Lesson9(hash)/Task4.py
@@ -9,5 +9,6 @@ print(hashlib.sha1(b'gfd' + b'Hello World!').hexdigest())
 s = hashlib.sha1(b'Hello World.').hexdigest()
 
 print(s.encode('utf-8'))
+print(bytes(s.encode('utf-8')))
 
 print(hashlib.sha1(b'fdgdfg' + bytes(s.encode('utf-8'))).hexdigest())
